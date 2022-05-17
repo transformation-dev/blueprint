7d2c6a704c047eba83491d1091954412b85bd3a9
Larry Maccherone <Larry@Maccherone.com>
2022-05-17 15:25:49 -0400

    Header approaching F7 version

---
+++ src/components/UnderlineTab.svelte
@@ -0,0 +1,35 @@
+
+<script>
+  
+  import { location } from 'svelte-spa-router'
+
+  export let label
+  export let href
+
+</script>
+
+<div>
+  <a href={`/#${href}`} class="tab-label-box link external">
+    {label}
+  </a>
+  <div class="tab-marker" class:selected={href === $location}></div>
+</div>
+
+<style>
+  
+  .tab-label-box {
+    margin-top: 8px;
+    margin-bottom: 0px;
+    height: var(--blueprint-tab-label-box-height) !important;
+  }
+
+  .tab-marker {
+    height: var(--blueprint-tab-marker-height);
+    background-color: var(--f7-theme-color);
+  }
+
+  .selected {
+    background-color: var(--blueprint-light-blue);
+  }
+
+</style>
