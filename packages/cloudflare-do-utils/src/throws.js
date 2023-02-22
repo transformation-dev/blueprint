de6c6fb1aed8c4fcb3adcc427336dedcfcfa7b38
Larry Maccherone <Larry@Maccherone.com>
2023-02-21 21:21:15 -0500

    Upgraded wrangler

--- packages/cloudflare-do-utils/src/throws.js
+++ packages/cloudflare-do-utils/src/throws.js
@@ -55,3 +55,24 @@ export function throwIfNotDag(o, currentPath = new Set()) {
   }
   return true
 }
+
+// Assumes that each node, including the root, is an object with an id property that is a string.
+// If the node also has a children property, it must be an array of nodes, recursively.
+export function throwIfNotDagArrays(o, currentPath = []) {  // TODO: Make this the active one once we revert tree to Arrays
+  const id = o.id ?? o.idString
+  throwUnless(id != null && typeof id === 'string', 'Each node, including the root, must have an id or idString property that is a string', 400)
+  throwIf(o.children != null && !Array.isArray(o.children), 'If present, the children property must be an Array', 400)
+  const newPath = [...currentPath, id]
+  // Note, we have to do the ancestor check in the next line outside of the for loop because we need to detect a cycle as soon as possible
+  // otherwise it will endlessly recurse down. I previously had an algorithm that found the leaves and then checked the ancestors of each leaf
+  // and that didn't work.
+  throwUnless(new Set(newPath).size === newPath.length, `Not a valid DAG. The path ${JSON.stringify(newPath)} contains duplicate ids`, 400)
+  if (o.children && o.children.length > 0) {
+    const siblings = []
+    for (const child of o.children) {
+      siblings.push(child.id ?? child.idString)
+      throwIfNotDag(child, newPath)
+    }
+    throwUnless(new Set(siblings).size === siblings.length, `The children of ${id}, ${JSON.stringify(siblings)} contain duplicate ids`, 400)
+  }
+}
