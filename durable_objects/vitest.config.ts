0b6beb54d2000c5af0201adf6c8a44a0ea2c87d1
Larry Maccherone <Larry@Maccherone.com>
2023-02-28 23:25:37 -0500

    Will codecov remove node_modules?

--- durable_objects/vitest.config.ts
+++ durable_objects/vitest.config.ts
@@ -1,29 +1,10 @@
 import path from 'path'
 import { defineConfig } from "vitest/config"
 
-const scriptPath = path.join(__dirname, "durable_objects/index.mjs")
+const scriptPath = path.join(__dirname, "index.mjs")
 
 export default defineConfig({
   test: {
-    coverage: {
-      // provider: 'c8',
-      provider: 'istanbul',
-      exclude: [
-        'node_modules/**',
-        'coverage/**',
-        'dist/**',
-        'packages/*/test{,s}/**',
-        '**/*.d.ts',
-        'cypress/**',
-        'test{,s}/**',
-        'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
-        '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
-        '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
-        '**/__tests__/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-        '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
-      ]
-    },
     // setupFiles: [
     //   path.join(__dirname, "test/_global-crypto.js"),
     // ],
